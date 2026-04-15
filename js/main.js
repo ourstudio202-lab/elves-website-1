90fb277ee4c995a52eea7f49dad8ab2c1deaaa80
ourstudio202-lab <our.studio202@gmail.com>
2026-04-15 18:49:48 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -17,3 +17,22 @@ menuBtn.addEventListener('click', function () {
         menuBtn.textContent = '+ Menu';
     }
 });
+// HERO PARALLAX
+const hero = document.querySelector('.hero');
+
+hero.addEventListener('mousemove', (e) => {
+    const { clientX, clientY } = e;
+
+    const x = (clientX / window.innerWidth) - 0.5;
+    const y = (clientY / window.innerHeight) - 0.5;
+
+    document.querySelectorAll('.blob').forEach((el, i) => {
+        const speed = (i + 1) * 20;
+        el.style.transform = `translate(${x * speed}px, ${y * speed}px)`;
+    });
+
+    document.querySelectorAll('.micro-text').forEach((el, i) => {
+        const speed = (i + 1) * 10;
+        el.style.transform = `translate(${x * speed}px, ${y * speed}px)`;
+    });
+});
